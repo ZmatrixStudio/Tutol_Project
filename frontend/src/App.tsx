3b82fccba1d3e735a3e520a04d1de52d66c18e86
ZmatrixStudio <2500021022@nttu.edu.vn>
2026-06-26 00:20:10 +0700

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import IndexPage from './pages/Login/Login'; // Trang chủ của bạn
+import LoginPage from './pages/Login/Login'; // Trang chủ của bạn
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Trang chủ index chỉ hiển thị giao diện của nó */}
-        <Route path="/" element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
